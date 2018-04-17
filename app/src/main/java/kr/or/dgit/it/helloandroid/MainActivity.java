a7d611711ef6fbca37526041167c38c89e34b7ed
Scarlett Lee <scarlett8152@gmail.com>
2018-04-17 09:43:33 +0900

    HelloAndroid 버튼 입력 - 수정

---
+++ app/src/main/java/kr/or/dgit/it/helloandroid/MainActivity.java
@@ -0,0 +1,20 @@
+package kr.or.dgit.it.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void mBtnClick(View view) {
+        Toast.makeText(this,"버튼을 눌렀습니다.하드코딩",Toast.LENGTH_LONG).show();
+        Toast.makeText(this,R.string.btnClickMsg,Toast.LENGTH_LONG).show();
+    }
+}
